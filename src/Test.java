854959a4a0f1987d4a4a02c7f1135b0d24df8fc1
Akkkai <summation007@163.com>
2020-07-14 17:17:34 +0800

    test

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+    public static void main(String[] args) {
+        SerializableUser();
+        DeSerializableUser();
+    }
+
+    private static void SerializableUser() {
+    }
+
+    private static void DeSerializableUser() {
+    }
+
+}
